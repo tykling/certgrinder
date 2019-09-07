acfa6905d3cc834821cb1a1074187453c1b874f2
Rudy (zarya) <git@gigafreak.net>
2019-09-07 21:42:16 +0200

    fixed pyyaml load deprecation https://github.com/yaml/pyyaml/wiki/PyYAML-yaml.load(input)-Deprecation

--- certgrinder.py
+++ certgrinder.py
@@ -51,7 +51,7 @@ class Certgrinder:
         """
         with open(configfile, "r") as f:
             try:
-                self.conf = yaml.load(f)
+                self.conf = yaml.load(f, Loader=yaml.FullLoader)
                 return True
             except Exception as E:
                 logger.exception("Unable to read config")
