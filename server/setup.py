5ba13d7eea4c22a4577264f95d9f3d72e7325124
Thomas Steen Rasmussen <thomas@gibfest.dk>
2021-01-18 15:52:00 +0100

    add python3.9

--- server/setup.py
+++ server/setup.py
@@ -24,6 +24,7 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
     ],
